55e5f66d76b9fe5051bdcbfc68eda25cb2a7a767
Lautaro Coria <lautarocoria97@gmail.com>
2022-07-04 18:54:12 -0300

    CV agregado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import React from 'react'
 import { Button, Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
-import { SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
+import { SiAdobeacrobatreader, SiGithub, SiGmail, SiLinkedin } from "react-icons/si";
 import  './NavBar.css'
 import {useTranslation} from  "react-i18next";
 export default function NavBar() {
@@ -20,6 +20,10 @@ export default function NavBar() {
       
       </Nav>
       <Nav >
+      <Nav.Link  href="https://mega.nz/file/RNIlEQhB#87EMgJksfLO-DDe0oMuSdD_Ikc_g4WNyxXRFWhEKwxc" target="_blank">
+          <SiAdobeacrobatreader />
+                            
+        </Nav.Link>
       <Nav.Link  href="https://www.linkedin.com/in/lautaro-coria-914848237/" target="_blank">
             <SiLinkedin  />
         </Nav.Link>
@@ -28,7 +32,10 @@ export default function NavBar() {
         </Nav.Link>
         <Nav.Link  href="https://github.com/lautacoria18" target="_blank">
           <SiGithub />
+                            
         </Nav.Link>
+        
+        
         <Button onClick={() => i18n.changeLanguage("es")}>ES</Button>
         <Button onClick={() => i18n.changeLanguage("en")}>EN</Button>
       </Nav>
